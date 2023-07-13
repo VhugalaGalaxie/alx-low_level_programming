007b80907d9dc80d81f89039ace8e9b128315990
VhugalaGalaxie <vhugalagabriel@gmail.com>
2023-07-13 14:50:53 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** 
+* main – A program that prints a line with puts function
+* Return : 0 (Success)
+*/
+Int main(void)
+{
+Puts(“\”Programming is like building a multilingual puzzle”);
+Return(0);
+}
+
