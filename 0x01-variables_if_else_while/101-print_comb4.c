5959d9a3e20c685eedcb92715c54ad23b1d78c68
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-15 20:24:48 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,23 +11,23 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		for (m = 48; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-				    putchar(m);
-				    putchar(l);
-				    if (n != 55 || m != 56)
-				    {
-				    	putchar (',');
-					    putchar (' ');
-				    }
-				}
-			}
-		}
+	for (m = 48; m < 58; m++)
+	{
+	for (l = 50; l < 58; l++)
+	{
+	if (l > m && m > n)
+	{
+	putchar(n);
+	putchar(m);
+	putchar(l);
+	if (n != 55 || m != 56)
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	}
 	}
 	putchar('\n');
 return (0);
