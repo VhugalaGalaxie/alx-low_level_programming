e36c4e893b62b2c7e5e891cb92af19a7bbb902cd
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-15 14:20:09 +0200

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main (void)
 	int n;
 	int RAND_MAX = 0;
 	strand (time(0));
-	n=rand () - RAND_MAX /2
+	n=rand () - RAND_MAX /2;
 	if ((n % 10) > 5) 
 {
 printF("Last digit of %d is %d and is greater than 5\n", n. % 10);
