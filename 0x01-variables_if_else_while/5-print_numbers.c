34eba0b3dc5be65e56306427ccf3eabb99540666
VhugalaGalaxie <vhugalagabriel@gmail.com>
2023-07-14 20:03:05 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints numbers from 0 to 9
+*return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		print("%d". i);
+	}
+	putchar('\n');
+	return (0):
+}
