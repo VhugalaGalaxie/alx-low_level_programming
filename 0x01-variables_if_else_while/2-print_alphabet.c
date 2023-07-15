09ceb3ad5174cae8761236f18b937b24ed8372a6
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-15 14:31:06 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,14 @@
 
 /**
 *main - prints the alphabet
-*return: always 0 (Success)
+*return: Always 0 (Success)
 */
 
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+	
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
