a440f15a24fbefe82ebc10a6c312d4aa01003694
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-15 14:03:59 +0200

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,19 +9,19 @@
 int main (void)
 {
 	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX/2;
+	srand (time(0));
+	n = srand() - RAND_MAX/2;
 	if (n > 0)
 {
-print ("%d is positive \n" .n);
+print("%d is positive \n" .n);
 }
 	else if (n == 0)
 {
-printf ("%d is zero \n" .n);
+printf("%d is zero \n" .n);
 }
 	else
 {
-printf ("%d is negative \n" .n);
+printf("%d is negative \n" .n);
 }
 	return (0);
 } 
