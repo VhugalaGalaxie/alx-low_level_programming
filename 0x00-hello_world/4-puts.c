44d53f60bf3b8b05c262c87231aa611060c885c6
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-16 18:35:44 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/** 
+/**
 * main – A program that prints a line with puts function
 * Return : 0 (Success)
 */
 
 int main(void)
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
