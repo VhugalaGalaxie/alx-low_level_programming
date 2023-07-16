af8a24bab31ce6d194bb533d82e5c991baab003d
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2023-07-16 19:35:48 +0200

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,17 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-	for (j = 0; j <= 99; j++)
+	for (j = i + 1; j <= 99; j++)
 	{
 	if (i < j)
 	{
-	putchar((i / 10) + 48);
-	putchar((i % 10) + 48);
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
 	putchar(' ');
-	putchar((j / 10) + 48);
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
 	if (i != 98 || j != 99)
+	continue;
 	{
 	putchar (',');
 	putchar(' ');
